1cefb10297cf372d5d1e00da9c489f4696c4404d
slowLee27J <drill27j@naver.com>
2025-04-02 17:29:52 +0900

    apple-ipad 0.0.05

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 장바구니!
 const basketStarterEl = document.querySelector('header .basket-starter');
 const basketEl = basketStarterEl.querySelector('.basket');
 
@@ -18,10 +19,44 @@ basketEl.addEventListener('click', function (event) {
 window.addEventListener('click', function () {
   hideBasket();
 });
-
 function showBasket() {
   basketEl.classList.add('show');
 }
 function hideBasket() {
   basketEl.classList.remove('show');
+}
+
+// 검색!
+const headerEl = document.querySelector('header')
+const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')]
+const searchWrapEl = headerEl.querySelector('.search-wrap')
+const searchStarterEl = headerEl.querySelector('.search-starter')
+const searchCloserEl = searchWrapEl.querySelector('.search-closer')
+const searchShadowEl = searchWrapEl.querySelector('.shadow')
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')]
+
+searchStarterEl.addEventListener('click', showSearch)
+searchCloserEl.addEventListener('click', hideSearch)
+searchShadowEl.addEventListener('click', hideSearch)
+
+function showSearch() {
+  headerEl.classList.add('searching')
+  document.documentElement.classList.add('fixed')
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
+  })
+  searchDelayEls.forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
+  })
+}
+function hideSearch() {
+  headerEl.classList.remove('searching')
+  document.documentElement.classList.remove('fixed')
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
+  })
+  searchDelayEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
+  })
+  searchDelayEls.reverse()
 }
