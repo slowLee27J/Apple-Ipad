6cc003e39dd83ed8d07cba3c0a314433e2a7a048
slowLee27J <drill27j@naver.com>
2025-04-17 15:47:19 +0900

    apple-ipad 1.0.0

--- js/main.js
+++ js/main.js
@@ -215,7 +215,8 @@ navigations.forEach(function (nav) {
 
   mapEl.innerHTML = /* html */ `
     <h3>
-      <span class = "text">${nav.title}</span>
+      <span class="text">${nav.title}</span>
+      <span class="icon">+</span>
     </h3>
     <ul>
       ${mapList}
@@ -228,3 +229,12 @@ navigations.forEach(function (nav) {
 
 const thisYearEl = document.querySelector('span.this-year')
 thisYearEl.textContent = new Date().getFullYear()
+
+
+const mapEls = document.querySelectorAll('footer .navigations .map')
+mapEls.forEach(function (el) {
+  const h3El = el.querySelector('h3')
+  h3El.addEventListener('click', function () {
+    el.classList.toggle('active')
+  })
+})
